18eacd2d3e407beaf9b6db1025ad7c3db4c99fae
gomi_ningen <korun24276@gmail.com>
2017-04-21 03:44:51 +0900

    Add struct Attachement

---
+++ Mastodon.API/Entities/Attachment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Attachment.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#attachment
+    /// </summary>
+    public struct Attachment
+    {
+        public string Id { get; }
+        public string Type { get; }
+        public string Url { get; }
+        public string RemoteUrl { get; }
+        public string PreviewUrl { get; }
+        public string TextUrl { get; }
+
+        public Attachment(string id, string type, string url, string remoteUrl, string previewUrl, string textUrl)
+        {
+            Id = id;
+            Type = type;
+            Url = url;
+            RemoteUrl = remoteUrl;
+            PreviewUrl = previewUrl;
+            TextUrl = textUrl;
+        }
+    }
+}

1fcc3668ac0d2ae170e426ee4d32e74f22c7b3c2
gomi_ningen <korun24276@gmail.com>
2017-04-21 03:56:35 +0900

    Add struct Error

---
+++ Mastodon.API/Entities/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Error.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#error
+    /// </summary>
+    public class Error
+    {
+        public string Description { get; }
+        public Error(string error)
+        {
+            Description = error;
+        }
+    }
+}

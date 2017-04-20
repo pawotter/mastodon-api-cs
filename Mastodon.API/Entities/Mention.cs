70f754f9b4ee7039def1c93564fb0a5c45b4b69a
gomi_ningen <korun24276@gmail.com>
2017-04-21 04:02:16 +0900

    Add struct Mention

---
+++ Mastodon.API/Entities/Mention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Mention.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#mention
+    /// </summary>
+    public class Mention
+    {
+        public string Url { get; }
+        public string Username { get; }
+        public string Acct { get; }
+        public string Id { get; }
+
+        public Mention(string url, string username, string acct, string id)
+        {
+            Url = url;
+            Username = username;
+            Acct = acct;
+            Id = id;
+        }
+    }
+}

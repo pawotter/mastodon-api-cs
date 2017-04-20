7a36550b32bd52abd19a324a3a96fc2c29fdb15a
gomi_ningen <korun24276@gmail.com>
2017-04-21 04:00:51 +0900

    Add struct Instance

---
+++ Mastodon.API/Entities/Instance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Instance.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#instance
+    /// </summary>
+    public class Instance
+    {
+        public string Uri { get; }
+        public string Title { get; }
+        public string Description { get; }
+        public string Email { get; }
+
+        public Instance(string uri, string title, string description, string email)
+        {
+            Uri = uri;
+            Title = title;
+            Description = description;
+            Email = email;
+        }
+    }
+}

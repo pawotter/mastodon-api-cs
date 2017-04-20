988133361101e7d207f708a582351bdfabb9b005
gomi_ningen <korun24276@gmail.com>
2017-04-21 03:36:57 +0900

    Add struct Application

---
+++ Mastodon.API/Entities/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Application.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#application
+    /// </summary>
+    public struct Application
+    {
+        public string Name { get; }
+        public string WebSite { get; }
+
+        public Application(string name, string webSite)
+        {
+            Name = name;
+            WebSite = webSite;
+        }
+    }
+}

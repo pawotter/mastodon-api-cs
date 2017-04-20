9080d89e0af5ceeceba179d669d15d1d9bff289a
gomi_ningen <korun24276@gmail.com>
2017-04-21 03:31:55 +0900

    initial commit

---
+++ Mastodon.API.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace Mastodon.API.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}

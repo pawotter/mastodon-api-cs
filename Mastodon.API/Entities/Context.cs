32f17cd31b71041043081e88963749619bbae84a
gomi_ningen <korun24276@gmail.com>
2017-04-21 04:45:53 +0900

    Add struct Context

---
+++ Mastodon.API/Entities/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Context.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#card
+    /// </summary>
+    public class Context
+    {
+        public IList<Status> Ancestors { get; }
+        public IList<Status> Descendants { get; }
+    }
+}

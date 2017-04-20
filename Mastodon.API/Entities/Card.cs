348705cf59a70060dd788a40602963be8a1b68f6
gomi_ningen <korun24276@gmail.com>
2017-04-21 03:53:40 +0900

    Add struct Card

---
+++ Mastodon.API/Entities/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Card.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#card
+    /// </summary>
+    public class Card
+    {
+        public string Url { get; }
+        public string Title { get; }
+        public string Description { get; }
+        public string Image { get; }
+
+        public Card(string url, string title, string description, string image)
+        {
+            Url = url;
+            Title = title;
+            Description = description;
+            Image = image;
+        }
+    }
+}

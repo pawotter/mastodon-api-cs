5b70899a76bf8113d26e7fbf15bc8818624ce742
gomi_ningen <korun24276@gmail.com>
2017-04-21 04:50:38 +0900

    Add struct Results

---
+++ Mastodon.API/Entities/Results.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Results.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#results
+    /// </summary>
+    public class Results
+    {
+        public IList<Account> Accounts { get; }
+        public IList<Status> Statuses { get; }
+        public IList<string> Hashtags { get; }
+
+        public Results(IList<Account> accounts, IList<Status> statuses, IList<string> hashtags)
+        {
+            Accounts = accounts;
+            Statuses = statuses;
+            Hashtags = hashtags;
+        }
+    }
+}

7eac504eafa8327bb4089388909878435320d160
gomi_ningen <korun24276@gmail.com>
2017-04-21 04:16:07 +0900

    Add struct Tag

---
+++ Mastodon.API/Entities/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Tag.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#tag
+    /// </summary>
+    public class Tag
+    {
+        public string Name { get; }
+        public string Url { get; }
+
+        public Tag(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+    }
+}

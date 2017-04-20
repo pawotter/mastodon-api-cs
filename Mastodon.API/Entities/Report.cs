d69931273f66a5e38880cbfdd19342790f1eee2f
gomi_ningen <korun24276@gmail.com>
2017-04-21 04:14:09 +0900

    Add struct Report

---
+++ Mastodon.API/Entities/Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Report.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#report
+    /// </summary>
+    public class Report
+    {
+        public string Id { get; }
+        public string ActionTaken { get; }
+
+        public Report(string id, string actionTaken)
+        {
+            Id = id;
+            ActionTaken = actionTaken;
+        }
+    }
+}

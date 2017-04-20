7db12ee72a79a8140e207f061247efd2eae757cd
gomi_ningen <korun24276@gmail.com>
2017-04-21 04:48:36 +0900

    Add struct Notification

---
+++ Mastodon.API/Entities/Notification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Mastodon.API
+{
+    /// <summary>
+    /// Notification.
+    /// https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#notification
+    /// </summary>
+    public class Notification
+    {
+        public string Id { get; }
+        public string Type { get; }
+        public string CreatedAt { get; }
+        public Account Account { get; }
+        public Status? Status { get; }
+
+        public Notification(string id, string type, string createdAt, Account account, Status? status)
+        {
+            Id = id;
+            Type = type;
+            CreatedAt = createdAt;
+            Account = account;
+            Status = status;
+        }
+    }
+}

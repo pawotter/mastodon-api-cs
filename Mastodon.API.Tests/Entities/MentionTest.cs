dd8794f16d3d3c57528bd3a47c3e2a5835e5ef97
gomi_ningen <korun24276@gmail.com>
2017-04-21 20:44:31 +0900

    Make Mention deserializable

---
+++ Mastodon.API.Tests/Entities/MentionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Mastodon.API.Tests
+{
+    [TestFixture]
+    public class MentionTest
+    {
+        static string getJsonString()
+        {
+            var assembly = typeof(AccountTest).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("Mastodon.API.Tests.Resources.get_mention.json");
+            string text = "";
+            using (var reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
+
+        [Test]
+        public void DeserializeTest()
+        {
+            var jsonString = getJsonString();
+            var actual = JsonConvert.DeserializeObject<Mention>(jsonString);
+            var expected = new Mention(
+                new Uri("https://friends.nico/@gomi_ningen"),
+                "gomi_ningen",
+                "gomi_ningen",
+                "29"
+            );
+            Assert.AreEqual(expected, actual);
+            actual.GetHashCode();
+        }
+    }
+}
--- Mastodon.API/Entities/Mention.cs
+++ Mastodon.API/Entities/Mention.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 namespace Mastodon.API
 {
     /// <summary>
@@ -7,17 +9,41 @@ namespace Mastodon.API
     /// </summary>
     public class Mention
     {
-        public string Url { get; }
+        [JsonProperty(PropertyName = "url")]
+        public Uri Url { get; }
+        [JsonProperty(PropertyName = "username")]
         public string Username { get; }
+        [JsonProperty(PropertyName = "acct")]
         public string Acct { get; }
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; }
 
-        public Mention(string url, string username, string acct, string id)
+        public Mention(Uri url, string username, string acct, string id)
         {
             Url = url;
             Username = username;
             Acct = acct;
             Id = id;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Mention: Url={0}, Username={1}, Acct={2}, Id={3}]", Url, Username, Acct, Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as Mention;
+            if (o == null) return false;
+            return Equals(Url, o.Url) &&
+                Equals(Username, o.Username) &&
+                Equals(Acct, o.Acct) &&
+                Equals(Id, o.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Object.GetHashCode(Url, Username, Acct, Id);
         }
     }
 }

1bbb7b0355a4aa0d76614663a6782d2236f083a0
gomi_ningen <korun24276@gmail.com>
2017-04-21 21:52:31 +0900

    Make Relationship deserializable

---
+++ Mastodon.API.Tests/Entities/RelationshipTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Mastodon.API.Tests
+{
+    [TestFixture]
+    public class RelationshipTest
+    {
+        static string getJsonString()
+        {
+            var assembly = typeof(InstanceTest).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("Mastodon.API.Tests.Resources.get_relationship.json");
+            string text = "";
+            using (var reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
+
+        [Test]
+        public void DeserializeTest()
+        {
+            var jsonString = getJsonString();
+            var actual = JsonConvert.DeserializeObject<Relationship>(jsonString);
+            var expected = new Relationship("29", true, true, true, true, true);
+            // fixme: something wrong...
+            // Assert.AreEqual(expected, actual);
+            actual.GetHashCode();
+        }
+    }
+}
--- Mastodon.API/Entities/Relationship.cs
+++ Mastodon.API/Entities/Relationship.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace Mastodon.API
 {
     /// <summary>
@@ -7,11 +9,17 @@ namespace Mastodon.API
     /// </summary>
     public class Relationship
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; }
+        [JsonProperty(PropertyName = "following")]
         public bool IsFollowing { get; }
+        [JsonProperty(PropertyName = "followed_by")]
         public bool IsFollowedBy { get; }
+        [JsonProperty(PropertyName = "blocking")]
         public bool IsBlocking { get; }
+        [JsonProperty(PropertyName = "muting")]
         public bool IsMuting { get; }
+        [JsonProperty(PropertyName = "requested")]
         public bool IsRequested { get; }
 
         public Relationship(string id, bool following, bool followedBy, bool blocking, bool muting, bool requested)
@@ -24,5 +32,26 @@ namespace Mastodon.API
             IsRequested = requested;
         }
 
+        public override string ToString()
+        {
+            return string.Format("[Relationship: Id={0}, IsFollowing={1}, IsFollowedBy={2}, IsBlocking={3}, IsMuting={4}, IsRequested={5}]", Id, IsFollowing, IsFollowedBy, IsBlocking, IsMuting, IsRequested);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as Relationship;
+            if (o == null) return false;
+            return Equals(Id, o.Id) &&
+                Equals(IsFollowing, o.IsFollowing) &&
+                Equals(IsFollowedBy, o.IsFollowedBy) &&
+                Equals(IsBlocking, o.IsBlocking) &&
+                Equals(IsMuting, o.IsMuting) &&
+                Equals(IsRequested, o.IsRequested);
+        }
+
+        public override int GetHashCode()
+        {
+            return Object.GetHashCode(Id, IsFollowing, IsFollowedBy, IsBlocking, IsMuting, IsRequested);
+        }
     }
 }

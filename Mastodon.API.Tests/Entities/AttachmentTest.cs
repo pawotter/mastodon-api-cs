8a5481dbe1a44882bb57025402ea6250b69d23a9
gomi_ningen <korun24276@gmail.com>
2017-04-21 20:08:33 +0900

    Make Attachement Deserializable

---
+++ Mastodon.API.Tests/Entities/AttachmentTest.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+
+namespace Mastodon.API.Tests
+{
+    [TestFixture]
+    public class AttachmentTest
+    {
+        static string getJsonString()
+        {
+            var assembly = typeof(InstanceTest).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("Mastodon.API.Tests.Resources.get_attachment.json");
+            string text = "";
+            using (var reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
+
+        [Test]
+        public void DeserializeTest()
+        {
+            var jsonString = getJsonString();
+            var actual = JsonConvert.DeserializeObject<Attachment>(jsonString);
+            var expected = new Attachment(
+                "39071",
+                "image",
+                new Uri("https://d2zoeobnny43zx.cloudfront.net/media_attachments/files/000/039/071/original/1a37136a4fe604b1.png?1492771668"),
+                new Uri("https://d2zoeobnny43zx.cloudfront.net/media_attachments/files/000/039/071/original/1a37136a4fe604b1.png?1492771668"),
+                new Uri("https://d2zoeobnny43zx.cloudfront.net/media_attachments/files/000/039/071/small/1a37136a4fe604b1.png?1492771668"),
+                new Uri("https://friends.nico/media/NB4b8WwFtCxC2ynwZis")
+            );
+            Assert.AreEqual(expected, actual);
+            actual.GetHashCode();
+        }
+    }
+}
--- Mastodon.API/Entities/Attachment.cs
+++ Mastodon.API/Entities/Attachment.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace Mastodon.API
 {
     /// <summary>
@@ -7,14 +9,20 @@ namespace Mastodon.API
     /// </summary>
     public class Attachment
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; }
+        [JsonProperty(PropertyName = "type")]
         public string Type { get; }
-        public string Url { get; }
-        public string RemoteUrl { get; }
-        public string PreviewUrl { get; }
-        public string TextUrl { get; }
+        [JsonProperty(PropertyName = "url")]
+        public Uri Url { get; }
+        [JsonProperty(PropertyName = "remote_url")]
+        public Uri RemoteUrl { get; }
+        [JsonProperty(PropertyName = "preview_url")]
+        public Uri PreviewUrl { get; }
+        [JsonProperty(PropertyName = "text_url")]
+        public Uri TextUrl { get; }
 
-        public Attachment(string id, string type, string url, string remoteUrl, string previewUrl, string textUrl)
+        public Attachment(string id, string type, Uri url, Uri remoteUrl, Uri previewUrl, Uri textUrl)
         {
             Id = id;
             Type = type;
@@ -23,5 +31,28 @@ namespace Mastodon.API
             PreviewUrl = previewUrl;
             TextUrl = textUrl;
         }
+
+        public override string ToString()
+        {
+            return string.Format("[Attachment: Id={0}, Type={1}, Url={2}, RemoteUrl={3}, PreviewUrl={4}, TextUrl={5}]", Id, Type, Url, RemoteUrl, PreviewUrl, TextUrl);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as Attachment;
+            if (o == null) return false;
+            return Equals(Id, o.Id) &&
+                Equals(Type, o.Type) &&
+                Equals(Url, o.Url) &&
+                Equals(RemoteUrl, o.RemoteUrl) &&
+                Equals(PreviewUrl, o.PreviewUrl) &&
+                Equals(TextUrl, o.TextUrl);
+        }
+
+        public override int GetHashCode()
+        {
+            return Object.GetHashCode(Id, Type, Url, RemoteUrl, PreviewUrl, TextUrl);
+        }
     }
 }
+

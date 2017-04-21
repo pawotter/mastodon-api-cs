38ac8e1a035af1ede11ba4cd76d66c2bbc45c496
gomi_ningen <korun24276@gmail.com>
2017-04-21 20:24:02 +0900

    Make Card deserializable

---
+++ Mastodon.API.Tests/Entities/CardTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using System;
+
+namespace Mastodon.API.Tests
+{
+    [TestFixture]
+    public class CardTest
+    {
+        static string getJsonString()
+        {
+            var assembly = typeof(InstanceTest).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("Mastodon.API.Tests.Resources.get_card.json");
+            string text = "";
+            using (var reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
+
+        [Test]
+        public void DeserializeTest()
+        {
+            var jsonString = getJsonString();
+            var actual = JsonConvert.DeserializeObject<Card>(jsonString);
+            var expected = new Card(
+                new Uri("http://seiga.nicovideo.jp/comic/20782"),
+                "ご注文はうさぎですか？ / Koi",
+                "喫茶ラビットハウスにやってきたココア。 実は、そこが彼女が住み込むことになっていた喫茶店で…。 すべて…",
+                new Uri("https://d2zoeobnny43zx.cloudfront.net/preview_cards/images/000/001/769/original/ogp_alternative.jpg?1492773335")
+            );
+            Assert.AreEqual(expected, actual);
+            actual.GetHashCode();
+        }
+    }
+}
--- Mastodon.API/Entities/Card.cs
+++ Mastodon.API/Entities/Card.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace Mastodon.API
 {
     /// <summary>
@@ -7,17 +9,41 @@ namespace Mastodon.API
     /// </summary>
     public class Card
     {
-        public string Url { get; }
+        [JsonProperty(PropertyName = "url")]
+        public Uri Url { get; }
+        [JsonProperty(PropertyName = "title")]
         public string Title { get; }
+        [JsonProperty(PropertyName = "description")]
         public string Description { get; }
-        public string Image { get; }
+        [JsonProperty(PropertyName = "image")]
+        public Uri Image { get; }
 
-        public Card(string url, string title, string description, string image)
+        public Card(Uri url, string title, string description, Uri image)
         {
             Url = url;
             Title = title;
             Description = description;
             Image = image;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Card: Url={0}, Title={1}, Description={2}, Image={3}]", Url, Title, Description, Image);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as Card;
+            if (o == null) return false;
+            return Equals(Url, o.Url) &&
+                Equals(Title, o.Title) &&
+                Equals(Description, o.Description) &&
+                Equals(Image, o.Image);
+        }
+
+        public override int GetHashCode()
+        {
+            return Object.GetHashCode(Url, Title, Description, Image);
         }
     }
 }

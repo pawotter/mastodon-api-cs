979ac487ead1c72d83b46e1c16664fe4c7b3a366
gomi_ningen <korun24276@gmail.com>
2017-04-22 04:44:05 +0900

    Amend IMastodonApi interface and DOCS

--- Mastodon.API/IMastodonApi.cs
+++ Mastodon.API/IMastodonApi.cs
@@ -7,7 +7,7 @@ namespace Mastodon.API
     public interface IMastodonApi
     {
         /// <summary>
-        /// Gets the OAuth token.
+        /// Return an OAuth token.
         /// DO NOT USE IN APP.
         /// </summary>
         /// <returns>OAuth token.</returns>
@@ -17,6 +17,20 @@ namespace Mastodon.API
         /// <param name="token">Token.</param>
         Task<Token> GetOAuthToken(string username, string password, OAuthAccessScope scope, CancellationToken? token = null);
 
+
+        /// <summary>
+        /// Returns an Account.
+        /// </summary>
+        /// <returns>The account.</returns>
+        /// <param name="id">Identifier.</param>
+        /// <param name="token">Token.</param>
         Task<Account> GetAccount(string id, CancellationToken? token = null);
+
+        /// <summary>
+        /// Returns the authenticated user's Account.
+        /// </summary>
+        /// <returns>The current account.</returns>
+        /// <param name="token">Token.</param>
+        Task<Account> GetCurrentAccount(CancellationToken? token = null);
     }
 }

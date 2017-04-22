827816bd56eff457d0419d692943abb7daea1cf6
gomi_ningen <korun24276@gmail.com>
2017-04-22 23:25:47 +0900

    Add test for OAuthAccessScope

---
+++ Mastodon.API.Tests/Entities/OAuthAccessScopeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Mastodon.API.Tests
+{
+    [TestFixture]
+    public class OAuthAccessScopeTest
+    {
+        [Test]
+        public void ValueTest()
+        {
+            Assert.AreEqual("read", OAuthAccessScope.of(OAtuhAccessScopeType.Read).Value);
+            Assert.AreEqual("write", OAuthAccessScope.of(OAtuhAccessScopeType.Write).Value);
+            Assert.AreEqual("follow", OAuthAccessScope.of(OAtuhAccessScopeType.Follow).Value);
+            Assert.AreEqual("read", OAuthAccessScope.of(OAtuhAccessScopeType.Read, OAtuhAccessScopeType.Read).Value);
+            Assert.AreEqual("read write", OAuthAccessScope.of(OAtuhAccessScopeType.Read, OAtuhAccessScopeType.Write).Value);
+            Assert.AreEqual("read write follow", OAuthAccessScope.of(OAtuhAccessScopeType.Read, OAtuhAccessScopeType.Write, OAtuhAccessScopeType.Follow).Value);
+            Assert.AreEqual("read write follow", OAuthAccessScope.of(OAtuhAccessScopeType.Read, OAtuhAccessScopeType.Write, OAtuhAccessScopeType.Follow, OAtuhAccessScopeType.Read).Value);
+        }
+    }
+}

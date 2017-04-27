6ae894fbf9da13c38d3d5e5c8e1a9a64566156bd
Jason Alsip <j.b.alsip@live.com>
2017-04-26 20:51:11 -0400

    HttpClient as an optional parameter, internally create a default HttpClient if it isn't provided

--- Mastodon.API.Tests/MastodonApiTest.cs
+++ Mastodon.API.Tests/MastodonApiTest.cs
@@ -4,17 +4,23 @@ using NUnit.Framework;
 
 namespace Mastodon.API.Tests
 {
-    //[TestFixture]
-    //public class MastodonApiTest
-    //{
-    //    [Test]
-    //    public void Test()
-    //    {
-    //        var http = new HttpClient();
-    //        var config = new MastodonApiConfig(new Uri("https://friends.nico"), "");
-    //        var api = new MastodonApi(config, http);
-    //        var account = api.GetFollowers("29", limit: 13).Result;
-    //        Console.WriteLine(account.Length);
-    //    }
-    //}
+    [TestFixture]
+    public class MastodonApiTest
+    {
+        //[Test]
+        //public void Test()
+        //{
+        //    var http = new HttpClient();
+        //    var config = new MastodonApiConfig(new Uri("https://friends.nico"), "");
+        //    var api = new MastodonApi(config, http);
+        //    var account = api.GetFollowers("29", limit: 13).Result;
+        //    Console.WriteLine(account.Length);
+        //}
+
+        [Test]
+        public void GetDefaultHttpClient_DoesNotReturnNull()
+        {
+            Assert.NotNull(MastodonApi.GetDefaultHttpClient());
+        }
+    }
 }
